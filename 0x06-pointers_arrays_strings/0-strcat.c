7abe23a8a2187a3e78ccd24f7f14535eed1daaec
etienontuk <jesteefoundation@gmail.com>
2023-03-03 13:09:45 -0800

    task zero of pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,44 @@
 #include "main.h"
 
 /**
- * strcat - concatenates the string printed to by @src, including the terminate null byte, to the end of the 
- * string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: the source string to be appended to @dest.
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
  *
- * Return: A pointer to the destination string @dest.
+ * Return: void
  */
-char *strcat(char *dest const char *src)
+
+char *_strcat(char *dest, char *src)
+
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+
+	{
+
+		i++;
+
+        }
+
+	j = 0;
+
+        while (src[j] != '\0')
+
+        {
+
+		dest[i] = src[j];
+
+                i++;
+
+		j++;
+
+        }
+
+	dest[i] = '\0';
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Etieno
- * Desc: Header file containing prototypes for all functions used in the 0x06, C = More pointers strings and arrays
- */
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -16,3 +10,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *a);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
