825562ca74ad85eb79c0c582435c8d3e59c117f6
etienontuk <jesteefoundation@gmail.com>
2023-03-03 12:43:55 -0800

    task zero of pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,11 @@
  */
 char *strcat(char *dest const char *src)
 {
-	_int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
+
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
